83c3d66f9e42a3b0a391dc7673dcee2516607c8d
shuhan liu <sueliu@shuhans-MacBook-Air.local>
2026-03-04 20:07:41 -0800

    fix eslint errors for vercel build

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,7 +4,6 @@ import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "./types";
 
 export function createClient() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return createBrowserClient<any>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
